72b05e74558c5a6afdc986709b08176f52494e38
Artur <artur459382@gmai.com>
2023-06-15 22:57:01 +0600

    Tackle 5 task

--- Solution5.cs
+++ Solution5.cs
@@ -1,6 +1,23 @@
 namespace LeetCodeTasks;
 
-public class Solution5
-{
-    
+public class Solution5 {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']') {
+                if (stack.Count == 0 || !IsMatchingBracket(stack.Pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.Count == 0;
+    }
+    private bool IsMatchingBracket(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
 }
