353593f93ab3d6f443206258a93b4965f2565e4b
Artur <artur459382@gmai.com>
2023-06-15 22:44:23 +0600

    Tackle 3 task

--- Solution3.cs
+++ Solution3.cs
@@ -1,6 +1,27 @@
 namespace LeetCodeTasks;
 
-public class Solution3
-{
-    
+public class Solution3 {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+        foreach (int num in nums1) {
+            if (countMap.ContainsKey(num)) {
+                countMap[num]++;
+            }
+            else {
+                countMap[num] = 1;
+            }
+        }
+
+        List<int> intersection = new List<int>();
+
+        foreach (int num in nums2) {
+            if (countMap.ContainsKey(num) && countMap[num] > 0) {
+                intersection.Add(num);
+                countMap[num]--;
+            }
+        }
+
+        return intersection.ToArray();
+    }
 }
